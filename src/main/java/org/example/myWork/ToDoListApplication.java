3cd69957dade1739f8339ae371cac744d7d39c50
ivkhard <ivhard20@gmail.com>
2021-12-22 17:26:42 +0300

    правка замечаний по 7му дз

--- src/main/java/org/example/myWork/ToDoListApplication.java
+++ src/main/java/org/example/myWork/ToDoListApplication.java
@@ -1,20 +1,14 @@
 package org.example.myWork;
 
 import lombok.RequiredArgsConstructor;
-import org.example.myWork.parser.CommandParser;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @RequiredArgsConstructor
-public class ToDoListApplication implements CommandLineRunner {
+public class ToDoListApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ToDoListApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
     }
 }
